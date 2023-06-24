5c9f095c4a8cf12c9a1812d6bbea7ef23f71bed5
TukaWarrior <luca.buetzberger@gmail.com>
2023-06-24 14:39:34 +0200

    Added games and TV Shows

---
+++ src/main/java/hftm/lucabuetzberger/Game.java
@@ -0,0 +1,94 @@
+package hftm.lucabuetzberger;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Game {
+    private SimpleStringProperty gameTitle = new SimpleStringProperty();
+    private SimpleStringProperty gameDeveloper = new SimpleStringProperty();
+    private SimpleIntegerProperty gameReleaseYear = new SimpleIntegerProperty();
+    private SimpleStringProperty gameGenre = new SimpleStringProperty();
+    private SimpleIntegerProperty gameRating = new SimpleIntegerProperty();
+    private SimpleIntegerProperty gamePlaytime = new SimpleIntegerProperty();
+
+
+    //Constructor
+    public Game(){
+    }
+
+    public Game(String bookTitle, String bookAuthor, int bookPages, int bookRating) {
+        this.setGameTitle(gameTitle);
+        this.setGameDeveloper(gameDeveloper);
+        this.setGameReleaseYear(gameReleaseYear);
+        this.setGameGenre(gameGenre);
+        this.setGamePlaytime(gamePlaytime);
+        this.setGameRating(gameRating);
+    }
+
+    // Game Title
+    public SimpleStringProperty gameTitleProperty(){
+        return this.gameTitle;
+    }
+    public String getGameTitle(){
+        return this.gameTitle.get();
+    }
+    public void setGameTitle(String gameTitle){
+        this.gameTitle.set(gameTitle);
+    }
+
+    // Game Developer
+    public SimpleStringProperty gameDeveloperProperty(){
+        return this.gameDeveloper;
+    }
+    public String getGameDeveloper(){
+        return this.gameDeveloper.get();
+    }
+    public void setGameDeveloper(String gameDeveloper){
+        this.gameDeveloper.set(gameDeveloper);
+    }
+
+    // Game ReleaseYear
+    public SimpleIntegerProperty gameReleaseYearProperty(){
+        return this.gameReleaseYear;
+    }
+    public Integer getGameReleaseYear(){
+        return this.gameReleaseYear.get();
+    }
+    public void setGameReleaseYear(Integer gameReleaseYear){
+        this.gameReleaseYear.set(gameReleaseYear);
+    }
+
+    // Game Genre
+    public SimpleStringProperty gameGenreProperty(){
+        return this.gameGenre;
+    }
+    public String getGameGenre(){
+        return this.gameGenre.get();
+    }
+    public void setGameGenre(String gameGenre){
+        this.gameGenre.set(gameGenre);
+    }
+
+    //Game Playtime
+    public SimpleIntegerProperty gamePlaytimeProperty(){
+        return this.gamePlaytime;
+    }
+    public Integer getGamePlaytime(){
+        return this.gamePlaytime.get();
+    }
+    public void setGamePlaytime(Integer gamePlaytime){
+        this.gamePlaytime.set(gamePlaytime);
+    }
+
+    //Game Rating
+    public SimpleIntegerProperty gameRatingProperty(){
+        return this.gameRating;
+    }
+    public Integer getGameRating(){
+        return this.gameRating.get();
+    }
+    public void setGameRating(Integer gameRating){
+        this.gameRating.set(gameRating);
+    }
+
+}
---
+++ src/main/java/hftm/lucabuetzberger/TVShow.java
@@ -0,0 +1,92 @@
+package hftm.lucabuetzberger;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class TVShow {
+    private SimpleStringProperty tvShowTitle = new SimpleStringProperty();
+    private SimpleStringProperty tvShowDirector = new SimpleStringProperty();
+    private SimpleIntegerProperty tvShowReleaseYear = new SimpleIntegerProperty();
+    private SimpleStringProperty tvShowGenre = new SimpleStringProperty();
+    private SimpleIntegerProperty tvShowEpisodes = new SimpleIntegerProperty();
+    private SimpleIntegerProperty tvShowRating = new SimpleIntegerProperty();
+
+    //Constructor
+    public TVShow(){
+    }
+
+    public TVShow(String bookTitle, String bookAuthor, int bookPages, int bookRating){
+        this.setTVShowTitle(tvShowTitle);
+        this.setTVShowDirector(tvShowDirector);
+        this.setTVShowReleaseYear(tvShowReleaseYear);
+        this.setTVShowGenre(tvShowGenre);
+        this.setTVShowLength(tvShowEpisodes);
+        this.setTVShowRating(tvShowRating);
+    }
+
+    // TVShow Title
+    public SimpleStringProperty tvShowTitleProperty(){
+        return this.tvShowTitle;
+    }
+    public String getTVShowTitle(){
+        return this.tvShowTitle.get();
+    }
+    public void setTVShowTitle(String tvShowTitle){
+        this.tvShowTitle.set(tvShowTitle);
+    }
+
+    // TVShow Director
+    public SimpleStringProperty tvShowDirectorProperty(){
+        return this.tvShowDirector;
+    }
+    public String getTVShowDirector(){
+        return this.tvShowDirector.get();
+    }
+    public void setTVShowDirector(String tvShowDirector){
+        this.tvShowDirector.set(tvShowDirector);
+    }
+
+    // TVShow ReleaseYear
+    public SimpleIntegerProperty tvShowReleaseYearProperty(){
+        return this.tvShowReleaseYear;
+    }
+    public Integer getTVShowReleaseYear(){
+        return this.tvShowReleaseYear.get();
+    }
+    public void setTVShowReleaseYear(Integer tvShowReleaseYear){
+        this.tvShowReleaseYear.set(tvShowReleaseYear);
+    }
+
+    // TVShow Genre
+    public SimpleStringProperty tvShowGenreProperty(){
+        return this.tvShowGenre;
+    }
+    public String getTVShowGenre(){
+        return this.tvShowGenre.get();
+    }
+    public void setTVShowGenre(String tvShowGenre){
+        this.tvShowGenre.set(tvShowGenre);
+    }
+
+    // TVShow Length
+    public SimpleIntegerProperty tvShowLengthProperty(){
+        return this.tvShowEpisodes;
+    }
+    public Integer getTVShowEpisodes(){
+        return this.tvShowEpisodes.get();
+    }
+    public void setTVShowLength(Integer tvShowEpisodes){
+        this.tvShowEpisodes.set(tvShowEpisodes);
+    }
+
+    // TVShow Rating
+    public SimpleIntegerProperty tvShowRatingProperty(){
+        return this.tvShowRating;
+    }
+    public Integer getTVShowRating(){
+        return this.tvShowRating.get();
+    }
+    public void setTVShowRating(Integer tvShowRating){
+        this.tvShowRating.set(tvShowRating);
+    }
+}
